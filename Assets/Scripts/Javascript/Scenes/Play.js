7417dd4aefe7ac0136ab8511ec4740180b506800
Baptiste Rosillon <batros.web@gmail.com>
2017-04-28 16:21:22 +0200

    initial commit

---
+++ Assets/Scripts/Javascript/Scenes/Play.js
@@ -0,0 +1,107 @@
+/**
+ * Create a new Scene
+ * <ul><li>Copy the content of this file in a new .js document.</li>
+ * <li>Save the new file in Assets/Javascript/Scenes/NameOfYourScene.js .</li>
+ * <li>In the index.html add below this comment <!-- Scene --> the line: 
+*                    "<script type="text/javascript" src="Assets/Scripts/Javascript/Scenes/NameOfYourGameObject.js"></script>"</li>
+ * <li>For create a new scene, use this instruction: "new Scene()".</li>
+ * </ul>
+ * <strong>To load your scene, use this instruction: "Application.LoadLevel(LevelName)".</strong>
+ * 
+ * @class
+ * 
+ * @return {Scene}
+ * */
+function PlayScene() 
+{
+	this.name = "Play";
+	this.GameObjects =[];
+	this.Groups = [];
+	this.Cameras = [];
+	this.CurrentCamera = null;
+	this.AlphaMask = null;
+	this.started = false;
+
+	this.WorldSize = new Vector(4096,4096);
+
+	this.gridWidth = 15;
+	this.Grid = new Grid(0,0, 900, this.gridWidth);
+
+	this.ObjectsGrid = [];
+
+	/**
+	 * Called at the instruction new Scene().
+	 * */
+	this.Awake = function() 
+	{
+		console.clear();
+		Print('System:Scene ' + this.name + " Created !");
+	}
+	
+	/**
+	 * Start the Scene and show a message in console or launch Update() if already started
+	 * Called at the first use of scene in game.
+	 * */
+	this.Start = function() 
+	{
+		if (!this.started) 
+		{
+			Time.SetTimeWhenSceneBegin();
+			// operation start
+
+			this.Grid.SetToMultidimensionnalArray();
+			this.ObjectsGrid = this.Grid.CloneArray(this.Grid.Tiles);
+
+			this.GameObjects.push(new PlayerObject(this.ObjectsGrid, this.gridWidth, this.Grid.caseLength, 2, 2));
+
+
+			this.GameObjects.push(new EnemyObject(this.ObjectsGrid, this.gridWidth, this.Grid.caseLength, 12, 12, this.GameObjects[0].PropagationObject));
+
+			this.started = true;
+			Print('System:Scene ' + this.name + " Started !");
+			Time.SetTimeWhenSceneLoaded();
+		}
+		this.Update();
+	}
+	/**
+	 * Start every GameObject, Group and apply the debug mode if asked
+	 * Called each frame,code game is here.
+	 * */
+	this.Update = function() 
+	{
+		if (!Application.GamePaused) 
+		{
+			for (var i = 0; i < this.GameObjects.length; i++) 
+			{
+				this.GameObjects[i].Start();
+			}
+			for (var i = 0; i < this.Groups.length; i++) 
+			{
+				this.Groups[i].Start();
+			}
+
+			this.Grid.DrawMultidimensionnalArray(true);
+		}
+		if (Application.debugMode) 
+		{
+			Debug.DebugScene();
+		}
+		this.GUI();
+	}
+	/**
+	 * Called each frame, code all the GUI here.
+	 * */
+	this.GUI = function() 
+	{
+		if (!Application.GamePaused) 
+		{
+			//Show UI
+		} 
+		else 
+		{
+			// Show pause menu
+		}
+	}
+
+	this.Awake()
+}
---
+++ Assets/Scripts/Javascript/Tools/Grid.js
@@ -0,0 +1,198 @@
+/**
+* Initializes the grid and obstacle
+* @class 
+*
+* @param {Number} _x - original position x of the grid
+* @param {Number} _y - original position y of the grid
+* @param {Number} _length - Desired length in pixel for the grid.
+* @param {Number} _cases - number of Cases desired.
+*
+* */ 
+function Grid(_x, _y, _length, _cases)
+{
+	this.x = _x;
+	this.y = _y;
+	this.length = _length;
+	this.cases = _cases;
+	this.caseLength = this.length / this.cases
+	this.Tiles = [];
+	this.TilesNew = [];
+
+	this.BestPath = null;
+
+	this.tilesCorrection =
+	{
+		y: -36,
+		width: 70
+	};
+
+/**
+*
+* Draw the grid
+* default black color 
+* 
+* */
+	this.Draw = function() 
+	{
+		ctx.strokeStyle = '#000000';
+		ctx.fillStyle = '#000000';
+		for (var i = 0; i * this.caseLength < this.length; i++) 
+		{
+			for (var j = 0; j * this.caseLength < this.length; j++) 
+			{
+				ctx.strokeRect(this.x + i * this.caseLength, this.y + j * this.caseLength, this.caseLength, this.caseLength);
+				// Draw Obstacles
+				if (this.Tiles[j * this.cases + i] == 1) {
+					ctx.fillStyle = '#000000';
+					ctx.fillRect(this.x + i * this.caseLength, this.y + j * this.caseLength, this.caseLength, this.caseLength);
+				}
+			}
+		}
+	}
+
+	this.DrawMultidimensionnalArray = function(grid = false) 
+	{
+		let tileImage = [null, null];
+
+		for (let x = 0; x < this.cases; x++)
+		{
+			for (let y = 0; y < this.cases; y++)
+			{
+				ctx.fillStyle = '#d9d7d6';
+
+				if (grid) {
+					ctx.strokeStyle="#66645e";
+					ctx.strokeRect(x*this.caseLength, y*this.caseLength, this.caseLength, this.caseLength);
+				}
+
+				switch (this.Tiles[x][y])
+				{
+					case 0:
+						continue;
+						break;
+					case 1:
+						ctx.fillStyle = "rgba(255,0,0, 0.2)";
+						break;
+					case 2:
+						ctx.fillStyle = "rgba(0,255,0, 0.2)";
+						break;
+					default:
+
+				}
+
+				if (tileImage[0]) {
+					ctx.drawImage(tileImage[0],
+								x*this.caseLength,
+								y*this.caseLength+this.tilesCorrection.y,
+								this.caseLength,
+								this.caseLength+this.tilesCorrection.width);
+					tileImage[0] = null;
+
+					if (tileImage[1]) {
+						ctx.drawImage(tileImage[1],
+							x*this.caseLength,
+							y*this.caseLength+this.tilesCorrection.y-25,
+							this.caseLength,
+							this.caseLength+this.tilesCorrection.width);
+						tileImage[1] = 0;
+					}
+				} else {
+					ctx.fillRect(x*this.caseLength, y*this.caseLength, this.caseLength, this.caseLength);
+				}
+			}
+		}
+	}
+
+	this.SetToMultidimensionnalArray = function()
+	{
+		for (let x = 0; x < this.cases; x++)
+		{
+			
+			this.Tiles[x] = [];
+			this.TilesNew[x] = [];
+
+			for (let y = 0; y < this.cases; y++)
+			{
+				this.Tiles[x][y] = 0;
+			}
+		}
+	}
+
+/**
+*
+* Get the mouse position on the Grid<br/>
+* exemple => position.x = 0 , position.y = 0. Return Index 0
+*
+**/	
+
+	this.GetMousePosition = function () 
+	{
+		var x = Input.MousePosition.x / this.caseLength |0;
+		var y = Input.MousePosition.y / this.caseLength |0;
+
+		return new Vector(x, y);	
+	}
+
+/**
+*
+* On debug mode, show the short path
+*
+**/	
+
+	this.ShowPathDebug = function() 
+	{
+		if (Application.debugMode) 
+		{
+			if(this.BestPath != null){
+				for (vector of this.BestPath) 
+				{
+					ctx.fillStyle = 'rgba(51,255,255,0.33)';
+					ctx.fillRect(this.x + vector.x * this.caseLength, this.y + vector.y * this.caseLength, this.caseLength, this.caseLength);
+				}	
+			}
+		}
+	}
+
+	this.CloneArray = function(Array)
+	{
+		let NewArray = [];
+
+		for (let i in Array) {
+			NewArray[i] = [];
+			NewArray[i] = Array[i].slice();
+		}
+
+		return NewArray;
+	}
+
+	this.ResetGrid = function(Array)
+	{
+		for (let x = 0; x < Array.length; x++)
+		{
+			for (let y = 0; y < Array[x].length; y++)
+			{
+				Array[x][y] = 0;
+			}
+		}
+	}
+
+	this.FillZone = function(startX, startY, endX, endY, id)
+	{
+		for (let x = startX; x < startX+endX; x++)
+		{
+			for (let y = startY; y < startY+endY; y++)
+			{
+				this.Tiles[x][y] = id;
+				this.TilesNew[x][y] = id;
+			}
+		}
+	}
+
+	this.GetCellType = function(x, y)
+	{
+		return this.Tiles[x][y];
+	}
+}
+
+
+	
